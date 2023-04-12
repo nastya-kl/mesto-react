1920b4aed4c88317ce4cfe7e4830f9525d5b7036
Anastasiia Klykova <qwerty03031998@yandex.ru>
2023-04-12 23:37:30 +0300

    Подключен АПИ инф-ции профиля

--- mesto/src/components/Main.js
+++ mesto/src/components/Main.js
@@ -1,28 +1,47 @@
 import React from 'react';
+import api from '../utils/Api';
 
 
 function Main(props) {
+  const [userName, setUserName] = React.useState();
+  const [userDescription, setUserDescription] = React.useState();
+  const [userAvatar, setUserAvatar] = React.useState();
+
+  React.useEffect(() => {
+    api.getUserInfo()
+      .then((data) => {
+        setUserAvatar(data.avatar);
+        setUserName(data.name);
+        setUserDescription(data.about);
+      })
+      .catch((err) => {
+        console.log(`Ошибка: ${err}`);
+      })
+  });
 
   return (
     <main className="main">
       <section className="profile">
         <div className="profile__info">
           <div className="profile__avatar-container" onClick={props.onEditAvatar}>
-            <img
+            <div
               className="profile__avatar"
               src="#"
               alt="Фото профиля"
-            />
+              style={{
+                backgroundImage: `url(${userAvatar})`
+              }}>
+            </div>
           </div>
           <div className="profile__text">
-            <h1 className="profile__name">Жак-Ив Кусто</h1>
+            <h1 className="profile__name">{userName}</h1>
             <button
               className="profile__edit-button"
               type="button"
               aria-label="Кнопка редактирования профиля"
               onClick={props.onEditProfile}
             ></button>
-            <p className="profile__description">Исследователь океана</p>
+            <p className="profile__description">{userDescription}</p>
           </div>
         </div>
         <button
